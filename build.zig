1bb0094822f3ad05d9664828e7c2277fa6b80c06
user <user@MacBook-Pro.local>
2026-04-19 10:25:54 +0800

    Make integration tests configurable

--- build.zig
+++ build.zig
@@ -30,6 +30,11 @@ pub fn build(b: *std.Build) void {
     unit_test_step.dependOn(&run_unit_tests.step);
 
     // zig build -Dtest-filter="..." integration_test
+    const integration_test_db_port = b.option(u16, "test-db-port", "Port for integration test database") orelse 3306;
+    const integration_test_db_user = b.option([]const u8, "test-db-user", "Username for integration test database") orelse "root";
+    const integration_test_db_password = b.option([]const u8, "test-db-password", "Password for integration test database") orelse "password";
+    const integration_test_db_name = b.option([]const u8, "test-db-name", "Database name for integration tests") orelse "mysql";
+
     const integration_tests = b.addTest(.{
         .root_module = b.createModule(.{
             .root_source_file = b.path("./integration_tests/main.zig"),
@@ -37,7 +42,13 @@ pub fn build(b: *std.Build) void {
             .optimize = optimize,
         }),
     });
+    const integration_test_options = b.addOptions();
+    integration_test_options.addOption(u16, "db_port", integration_test_db_port);
+    integration_test_options.addOption([]const u8, "db_user", integration_test_db_user);
+    integration_test_options.addOption([]const u8, "db_password", integration_test_db_password);
+    integration_test_options.addOption([]const u8, "db_name", integration_test_db_name);
     integration_tests.root_module.addImport("myzql", myzql);
+    integration_tests.root_module.addOptions("integration_test_options", integration_test_options);
     if (test_filter) |t| integration_tests.filters = t;
 
     // zig build [install]
--- integration_tests/config.zig
+++ integration_tests/config.zig
@@ -1,11 +1,19 @@
 const myzql = @import("myzql");
 const Config = myzql.config.Config;
+const integration_test_options = @import("integration_test_options");
+
+const db_user: [:0]const u8 = integration_test_options.db_user[0..integration_test_options.db_user.len :0];
+const db_name: [:0]const u8 = integration_test_options.db_name[0..integration_test_options.db_name.len :0];
 
 pub const test_config: Config = .{
-    .password = "password",
+    .username = db_user,
+    .password = integration_test_options.db_password,
+    .address = myzql.config.Address.localhost(integration_test_options.db_port),
 };
 
 pub const test_config_with_db: Config = .{
-    .password = "password",
-    .database = "mysql",
+    .username = db_user,
+    .password = integration_test_options.db_password,
+    .database = db_name,
+    .address = myzql.config.Address.localhost(integration_test_options.db_port),
 };
